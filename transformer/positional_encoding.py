3140056ee3b49d1bcd2df9bc0fa0f861d3dfc44c
abhiramkandiyana <abhiramkandiyana8@gmail.com>
2025-02-21 10:28:26 -0500

    positional encoding completed : added clarifying comments.

--- transformer/positional_encoding.py
+++ transformer/positional_encoding.py
@@ -8,13 +8,27 @@ class PositionalEncoding(nn.Module):
         super().__init__()
         self.seq_len = seq_len
         self.d_model = d_model
-        self.pos_enc = torch.zeros(self.seq_len, self.d_model)
-        self.register_buffer("p_enc", self.pos_enc)
         self.dropout = nn.Dropout(p=p_dropout)
+
+        # initialize pos_enc matrix with zeroes
+        self.pos_enc = torch.zeros(self.seq_len, self.d_model)
+
+        # add pos_enc to register so Pytorch knows that it belongs to this class even if it doesn't require grads
+        self.register_buffer("p_enc", self.pos_enc)
+
+        # get a tensor of all the positions in the sequence.
+        # Unsqueeze positions so it can broadcasted when multiplied with dims
         positions = torch.arange(seq_len).unsqueeze(1)
+
+        # get a tensor of all the dimensions
         dims = torch.arange(d_model)
+
+        # calculate the common denominator once so you can directly use it for all positions
         div_term = 1/torch.pow(10000, dims/self.d_model)
+
         self.pos_enc = positions * div_term
+
+        # apply sin to even positions (starting from 0) and cos to odd positions.
         self.pos_enc[:, 0::2] = torch.sin(self.pos_enc[:, 0::2])
         self.pos_enc[:, 1::2] = torch.cos(self.pos_enc[:, 1::2])
 
@@ -26,6 +40,7 @@ class PositionalEncoding(nn.Module):
         self.pos_enc: shape = (seq_len, d_model)
         '''
 
+        # move pos_enc to the GPU where X is before adding it to X
         x = x + self.pos_enc[:x.shape[1], :].to(x.device)
         return self.dropout(x)
 
